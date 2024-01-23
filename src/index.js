102dbf25432e811b526bd5a16e1b43d84c03d2d4
Sana Parveen <misana348@gmail.com>
2024-01-23 13:48:26 +0530

    Routes and data saving with multer error resolutions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
   path: "./.env",
@@ -17,6 +18,7 @@ connectDB()
   .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
   });
+
 
 // import express from "express"
 // const app = express()
